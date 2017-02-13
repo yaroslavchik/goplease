802927614480682192c6cda16b39a6b2ef077149
YAROSLAV <yaovtk@mail.ru>
2017-02-13 20:03:03 +0200

    added first class BasicGame

---
+++ ConsoleApplication1/ConsoleApplication1/Game/BasicGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Game
+{
+    public abstract class BasicGame
+    {
+        protected string Description = "";
+
+        public string GetDescription()
+        {
+            return Description;
+        }
+
+        public abstract double GetSize();
+
+    }
+
+}
