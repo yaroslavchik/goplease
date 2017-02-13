1c80fce9e53a6988fbdd4fe22e4798d829fc65f5
YAROSLAV <yaovtk@mail.ru>
2017-02-13 21:58:10 +0200

    completed decorator

--- ConsoleApplication1/ConsoleApplication1/Game/Fishing.cs
+++ ConsoleApplication1/ConsoleApplication1/Game/Fishing.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Game
+{
+    class Fishing : BasicGame
+    {
+        public Fishing()
+        {
+            Description = "The greate simulator about fishing";
+        }
+
+        public override double GetSize()
+        {
+            return 110;
+        }
+
+    }
+}
