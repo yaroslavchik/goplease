7e4d9df68ead747d6c186b8812223b4e1be6754c
YAROSLAV <yaovtk@mail.ru>
2017-02-13 20:03:51 +0200

    added class FightClub

--- ConsoleApplication1/ConsoleApplication1/Game/FightClub.cs
+++ ConsoleApplication1/ConsoleApplication1/Game/FightClub.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Game
+{
+    class FightClub : BasicGame
+    {
+        public FightClub()
+        {
+            Description = "Game about street fighting";
+        }
+
+        public override double GetSize()
+        {
+            return 150;
+        }
+
+    }
+}
